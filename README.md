abcdcec36f8a079c509ade05d81530e5c0495bd7
Idris Khan <idris.khan303@gmail.com>
2021-05-14 22:16:23 +0100

    Basic directives and interpolation covered

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Vue.js v3 course
 
-## Section-1 starter project
+## Section-1 Starter Project
 This directory will contain a simple example of a Vue.js project, this is to get a feel how Vue interacts and works
+
+## Section-2 Basics
+Here we go over basic state management, callbacks and injecting the functionality into the HTML via Vue directives
---
+++ Section-2-basics/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: 'Master Vue and build amazing apps!',
+      courseGoalHTML: '<h2>HTML Vue!</h2>',
+      vueLink: 'https://vuejs.org/',
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      }
+      return this.courseGoalB;
+    },
+  },
+});
+
+app.mount('#user-goal');
