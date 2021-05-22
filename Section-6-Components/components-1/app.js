4bb5d66a346d5cb4787a4d079f4cf96f57d48953
Idris Khan <idris.khan303@gmail.com>
2021-05-22 15:35:29 +0100

    finished component introduction

--- Section-6-Components/components-1/app.js
+++ Section-6-Components/components-1/app.js
@@ -8,8 +8,45 @@ const app = Vue.createApp({
           phone: '01234 5678 991',
           email: 'manuel@localhost.com',
         },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '09876 543 221',
+          email: 'julie@localhost.com',
+        },
       ],
     };
+  },
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleFriend">
+      {{ isToggleFriend ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-show="isToggleFriend">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      isToggleFriend: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleFriend() {
+      this.isToggleFriend = !this.isToggleFriend;
+    },
   },
 });
 
