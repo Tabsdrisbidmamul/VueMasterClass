f4147f5f33052b82fb318226663ba3f9af80961d
Idris Khan <ricepoo123@gmail.com>
2021-05-25 11:11:08 +0100

    Start of Vue CLI components project, we have now fully got back to the point of where we were in section-6

---
+++ Section-7-Vue-CLI/Vue-Project-1/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleFriend">
+      {{ isToggleFriend ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-show="isToggleFriend">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isToggleFriend: true,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleFriend() {
+      this.isToggleFriend = !this.isToggleFriend;
+    },
+  },
+};
+</script>
