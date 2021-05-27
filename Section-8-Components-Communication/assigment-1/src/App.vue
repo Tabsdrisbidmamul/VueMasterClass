b3e0d458046d140cb3aab5cf071d15505c570b0c
Idris Khan <idris.khan303@gmail.com>
2021-05-27 14:19:53 +0100

    start of assignment

---
+++ Section-8-Components-Communication/assigment-1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <active-user user-name="Idris" :age="20" />
+  </section>
+</template>
+
+<script>
+import ActiveUser from './components/ActiveUser';
+
+export default {
+  components: {
+    'active-user': ActiveUser,
+  },
+  data() {
+    ActiveUser;
+    return {
+      activeUser: {},
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ Section-8-Components-Communication/assigment-1/src/components/ActiveUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2 class="header header--h2">{{ userName }}</h2>
+  <h3 class="header header--h3">{{ age }}</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
