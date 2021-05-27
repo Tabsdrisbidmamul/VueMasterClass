246255476660c595cdc55ad9168e3a01bf2109a6
Idris Khan <idris.khan303@gmail.com>
2021-05-27 14:22:13 +0100

    Start of assignment

--- Section-8-Components-Communication/assigment-1/src/App.vue
+++ Section-8-Components-Communication/assigment-1/src/App.vue
@@ -20,10 +20,12 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
 
 * {
   box-sizing: border-box;
 }
+
+
 </style>
