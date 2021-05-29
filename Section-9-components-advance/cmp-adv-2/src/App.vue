3110c174ae4d40bdb1b2255e2cb48037e5465e5e
Idris Khan <ricepoo123@gmail.com>
2021-05-29 16:49:37 +0100

    more on slots and dynamic components"

---
+++ Section-9-components-advance/cmp-adv-2/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goal')">Active Goal</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"> </component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import ActiveGoal from './components/ActiveGoal';
+import ManageGoals from './components/ManageGoals';
+
+export default {
+  components: {
+    TheHeader,
+    ActiveGoal,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: 'active-goal',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap');
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ Section-9-components-advance/cmp-adv-2/src/components/ManageGoals.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Manage Goal</h2>
+    <input type="text" v-model="goalText" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please enter a input</p>
+      <button @click="confirmGoal">Okay!</button>
+    </error-alert>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert';
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+      goalText: '',
+    };
+  },
+  methods: {
+    setGoal() {
+      if (this.goalText === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmGoal() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
