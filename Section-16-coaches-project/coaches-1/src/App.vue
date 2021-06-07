22170c57441a6df522802c00cfeb9fe458e7851c
Idris Khan <ricepoo123@gmail.com>
2021-06-07 22:19:26 +0100

    Router created and empty components made, the router-view has been added into the app.vue and the-header with router navigation has been made

---
+++ Section-16-coaches-project/coaches-1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.2rem;
+  margin: 0;
+  padding: 0;
+}
+</style>
