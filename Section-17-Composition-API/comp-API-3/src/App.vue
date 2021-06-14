9abbeef07714e29d0e4d89c07f5eaafe527a5701
Idris Khan <idris.khan303@gmail.com>
2021-06-14 18:01:53 +0100

    wrote the translation of termplate refs from options API to Composition API

---
+++ Section-17-Composition-API/comp-API-3/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="container">
+    <h2>{{ fullName }}</h2>
+    <h3>{{ age }}</h3>
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref, computed, watch } from 'vue';
+// import { ref, reactive, computed, watch } from 'vue';
+
+export default {
+  setup() {
+    // const user = reactive({
+    //   userName: 'Maximilian',
+    //   age: 31
+    // });
+    // const user = reactive({
+    //   age: 31
+    // });
+
+    const firstName = ref('');
+    const lastName = ref('');
+    const lastNameInput = ref(null);
+    const fullName = computed(() => {
+      return `${firstName.value} ${lastName.value}`;
+    });
+
+    const age = ref(31);
+
+    // watch(
+    //   () => user.age,
+    //   (newVal, oldVal) =>
+    //     console.log(`New Value: ${newVal}`, `Old Value: ${oldVal}`)
+    // );
+
+    watch([age, fullName], (newValues, oldValues) => {
+      console.log(`New Age: ${newValues[0]}`, `Old Age: ${oldValues[0]}`);
+      console.log(`New Name: ${newValues[1]}`, `Old Name: ${oldValues[1]}`);
+    });
+
+    // const setFirstName = event => {
+    //   firstName.value = event.target.value;
+    // };
+
+    // const setLastName = event => {
+    //   lastName.value = event.target.value;
+    // };
+
+    const setAge = () => {
+      age.value = 32;
+    };
+
+    const setLastName = () => {
+      lastName.value = lastNameInput.value.value;
+    };
+
+    return {
+      age,
+      fullName,
+      setAge,
+      setLastName,
+      firstName,
+      lastNameInput
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
