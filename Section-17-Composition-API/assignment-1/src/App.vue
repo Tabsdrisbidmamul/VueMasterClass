a415834363ddcea5c49292606193da01efcaa697
Idris Khan <idris.khan303@gmail.com>
2021-06-14 17:22:34 +0100

    completed the assigment

--- Section-17-Composition-API/assignment-1/src/App.vue
+++ Section-17-Composition-API/assignment-1/src/App.vue
@@ -2,9 +2,11 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="courseRefObj.isCourseGoalVisible">
+    {{ courseRefObj.courseGoal }}
+  </h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleCourseGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +15,47 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+// import { reactive } from 'vue';
+
 export default {
-  
-}
+  setup() {
+    // const courseGoalRed = ref(
+    //   'Finish this course and start on fireblogs project then onto next.js'
+    // );
+    // const isCourseGoalVisibleRed = ref(true);
+
+    // const toggleCourseGoal = () => {
+    //   isCourseGoalVisibleRed.value = !isCourseGoalVisibleRed.value;
+    // };
+
+    const courseRefObj = ref({
+      courseGoal:
+        'Finish this course and start on fireblogs project then onto next.js',
+      isCourseGoalVisible: true
+    });
+
+    const toggleCourseGoal = () => {
+      courseRefObj.value.isCourseGoalVisible = !courseRefObj.value
+        .isCourseGoalVisible;
+    };
+
+    // const courseReactiveObj = reactive({
+    //   courseGoal:
+    //     'Finish this course and start on fireblogs project then onto next.js',
+    //   isCourseGoalVisible: true
+    // });
+
+    // const toggleCourseGoal = () => {
+    //   courseReactiveObj.isCourseGoalVisible = !courseReactiveObj.isCourseGoalVisible;
+    // };
+
+    return {
+      courseRefObj,
+      toggleCourseGoal
+    };
+  }
+};
 </script>
 
 <style>
