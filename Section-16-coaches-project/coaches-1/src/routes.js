0b0cd414a57c50f1ab2ad9fd4151db4587ca45ab
Idris Khan <idris.khan303@gmail.com>
2021-06-14 13:48:37 +0100

    Added firebase config and built the project, and is deployed on firebase

--- Section-16-coaches-project/coaches-1/src/routes.js
+++ Section-16-coaches-project/coaches-1/src/routes.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachDetails from './pages/coaches/CoachDetails.vue';
-import CoachContact from './pages/requests/ContactCoach.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
-import NotFound from './pages/NotFound.vue';
+// import CoachesList from './pages/coaches/CoachesList.vue';
+// import CoachDetails from './pages/coaches/CoachDetails.vue';
+// import CoachContact from './pages/requests/ContactCoach.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
+// import NotFound from './pages/NotFound.vue';
 
 import store from './store/index';
 
@@ -14,27 +14,37 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches',
+      component: () => import('./pages/coaches/CoachesList.vue')
+    },
     {
       path: '/coaches/:id',
-      component: CoachDetails,
+      component: () => import('./pages/coaches/CoachDetails.vue'),
       props: true,
       children: [
-        { path: 'contact', component: CoachContact } // /coaches/c1/contact
+        {
+          path: 'contact',
+          component: () => import('./pages/requests/ContactCoach.vue')
+        } // /coaches/c1/contact
       ]
     },
     {
       path: '/register',
-      component: CoachRegistration,
+      component: () => import('./pages/coaches/CoachRegistration.vue'),
       meta: { requiresAuth: true }
     },
     {
       path: '/requests',
-      component: RequestsReceived,
+      component: () => import('./pages/requests/RequestsReceived.vue'),
       meta: { requiresAuth: true }
     },
-    { path: '/auth', component: UserAuth, meta: { requiresUnauth: false } },
-    { path: '/:notFound(.*)', component: NotFound }
+    {
+      path: '/auth',
+      component: () => import('./pages/auth/UserAuth.vue'),
+      meta: { requiresUnauth: false }
+    },
+    { path: '/:notFound(.*)', component: () => import('./pages/NotFound.vue') }
   ],
   scrollBehavior(_, _2, savedPosition) {
     if (savedPosition) {
