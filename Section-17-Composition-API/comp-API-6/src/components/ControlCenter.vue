7469c1ab3f08e3b1c204f417976c2ca64d808878
Idris Khan <idris.khan303@gmail.com>
2021-06-15 17:28:40 +0100

    Vuex composition API funamentals learnt, really its was one hook, everything else still works the same

--- Section-17-Composition-API/comp-API-6/src/components/ControlCenter.vue
+++ Section-17-Composition-API/comp-API-6/src/components/ControlCenter.vue
@@ -3,11 +3,17 @@
 </template>
 
 <script>
+import { useStore } from 'vuex';
+
 export default {
   setup() {
-    function inc() {}
+    const store = useStore();
+
+    function inc() {
+      store.dispatch('increment');
+    }
 
     return { inc };
-  },
+  }
 };
 </script>
--- Section-17-Composition-API/comp-API-6/src/components/TheCounter.vue
+++ Section-17-Composition-API/comp-API-6/src/components/TheCounter.vue
@@ -3,11 +3,17 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
 export default {
   setup() {
-    const counter = 0;
+    const store = useStore();
+    const counter = computed(() => {
+      return store.getters.counter;
+    });
 
     return { counter };
-  },
+  }
 };
 </script>
